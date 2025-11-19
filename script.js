2b12d5c66374ad962bba4505a45901849be7498e
yofmusicoff-cmd <yof.music.off@gmail.com>
2025-11-20 01:18:36 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showArticle(id) {
- document.querySelectorAll('article').forEach(a => a.classList.remove('active'));
- document.getElementById(id).classList.add('active');
- document.querySelectorAll('nav button').forEach(b => b.classList.remove('active'));
- document.getElementById(id + 'Btn').classList.add('active');
-}
